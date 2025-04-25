dac0c2be2fe9a660f3f731eea862576f0b34a697
Rufus B <127842229+rufusbiggs@users.noreply.github.com>
2025-04-25 12:33:30 +0100

    laser negative side hit bug fix

--- game.py
+++ game.py
@@ -40,7 +40,7 @@ def check_laser_hit(alien_row, alien_height, laser_pos):
     # laser_index in relation to alien_row = x - 5
 
     laser_xpos_idx = laser_pos[0] - 5
-    if laser_xpos_idx >= game_width - 10:
+    if laser_xpos_idx >= game_width - 10 or laser_xpos_idx < 0:
         return False
     
     if alien_row[laser_xpos_idx] == 1:
